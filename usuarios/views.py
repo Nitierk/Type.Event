8769b29c1a84426017963acf32c70648599ea6e9
Nitierk <105940525+Nitierk@users.noreply.github.com>
2023-04-16 18:10:12 -0300

    Att, ta bug

---
+++ usuarios/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+
+def OPA(request):
+    if request.method == "GET":
+        return render(request, 'opa.html')
