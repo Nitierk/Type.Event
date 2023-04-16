6f80869b055f2275fcbc0c3974f34f6b9b9e223a
Nitierk <105940525+Nitierk@users.noreply.github.com>
2023-04-16 20:48:14 -0300

    Method POST

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def cadastro(request):
     if request.method == "GET":
         return render(request, "cadastro.html")
+    elif request.method == "POST":
+        return HttpResponse('Testando')
